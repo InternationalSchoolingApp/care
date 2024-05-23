1529c62f40a88b5feb1b456521014e713b756e09
Shoaib Ansari <app@internationalschooling.org>
2024-05-23 14:47:23 +0530

    check teake pull

--- app/constants/ImagePath.js
+++ app/constants/ImagePath.js
@@ -1,8 +1,9 @@
 const ImagePath = {
-  LOGO: "/logo.webp",
-  LEFT_SVG: "/left.svg",
-  MAIN_BANNER: "/main_banner.webp",
-  HERO_IMG: "/why_is_hero_img.png",
-  WHY_IS_IMG_2: "/why_is_img2.png",
+    LOGO: "/logo.webp",
+    LEFT_SVG: "/left.svg",
+    MAIN_BANNER: "/main_banner.webp",
+    HERO_IMG: "/why_is_hero_img.png",
+    WHY_IS_IMG_2: "/why_is_img2.png",
+    IMAGE_GIRL: '/main_page_girl.webp'
 };
 export default ImagePath;
