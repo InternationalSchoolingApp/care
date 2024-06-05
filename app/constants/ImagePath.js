32260896a14afb58d65cdb14b1e69a7bde5c9d8e
Shoaib Ansari <app@internationalschooling.org>
2024-06-05 14:48:35 +0530

    check out please for performance

--- app/constants/ImagePath.js
+++ app/constants/ImagePath.js
@@ -1,7 +1,7 @@
 const ImagePath = {
-  LOGO: "/logo.webp",
+  LOGO: "/logo_c.webp",
   LEFT_SVG: "/left.svg",
-  MAIN_BANNER: "/main_banner.webp",
+  MAIN_BANNER: "/main_banner_c.webp",
   HERO_IMG: "/why_is_hero_img.png",
   WHY_IS_IMG_2: "/why_is_img2.png",
   EXTRA_IMG: "/extra_img.png",
