bce418204d84b6f0c704f0da53f3a88ee67f64de
Shoaib Ansari <app@internationalschooling.org>
2024-06-07 22:59:08 +0530

    mobile menu dynamic

--- app/components/CommonHeader.js
+++ app/components/CommonHeader.js
@@ -5,6 +5,9 @@ import Image from "next/image";
 import ImagePath from "@constants/ImagePath";
 import Menu from "./Menu";
 import UrlPath from "@constants/UrlPath";
+import dynamic from "next/dynamic";
+
+const DynamicMobileMenu = dynamic(() => import("./MobileMenu.js"), { ssr: false });
 
 const CommonHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -49,21 +52,8 @@ const CommonHeader = () => {
           <Menu menu={menu} items={menuItem} />
         </div>
       </div>
-      {isOpen && (
-        <div className="flex flex-col lg:hidden w-full rounded px-2 py-6  ">
-          {menuItem.map((item, index) => {
-            return (
-              <Link
-                key={index}
-                className="hover:text-blue-500 font-medium w-full mb-2  py-2 px-4 rounded hover:bg-base-200"
-                href={item.link}
-              >
-                {item.title}
-              </Link>
-            );
-          })}
-        </div>
-      )}
+      {isOpen && <DynamicModal menuItem={menuItem} />
+      }
     </header>
   );
 };
